62f034844e4964bb19559d247e6f0d730056d763
Jeeshan-Shrestha <shresthajeesan@gmail.com>
2026-02-20 12:03:30 +0545

    backend folder structures and init

---
+++ backend/Servify/src/test/java/com/example/Servify/ServifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Servify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
